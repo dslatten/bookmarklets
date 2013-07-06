4f8652823b13dbaea16eef9445daf78ce0640871
Darren Slatten <darrenslatten@gmail.com>
2013-07-06 14:59:34 -0500

    Added 800px

---
+++ 800px/800px.js
@@ -0,0 +1,8 @@
+(function() {
+    var html = document.getElementsByTagName('html')[0];
+    html.style.width = '800px';
+    html.style.margin = '0 auto';
+    var body = document.getElementsByTagName('body')[0];
+    body.style.width = '800px';
+    body.style.margin = '0 auto 400px auto';
+})();
