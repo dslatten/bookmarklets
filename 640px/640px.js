d5a867fa205cac76ca9000be4a86e71240936b08
Darren Slatten <darrenslatten@gmail.com>
2013-10-30 07:31:43 -0500

    Added 640px.js

---
+++ 640px/640px.js
@@ -0,0 +1,12 @@
+(function() {
+    var html = document.getElementsByTagName('html')[0];
+    html.style.width = '640px';
+    html.style.height = '100%';
+    html.style.margin = '0 auto';
+    html.style.padding = '0';
+    var body = document.getElementsByTagName('body')[0];
+    body.style.width = '640px';
+    body.style.height = '100%';
+    body.style.margin = '0 auto 400px auto';
+    body.style.padding = '0';
+})();
